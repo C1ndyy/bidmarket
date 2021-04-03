78ef0f0a3bef32d20b382ebb30caaa088d2a1606
C1ndyy <cindx.xu@gmail.com>
2021-04-02 22:04:59 -0600

    set up authentication routes

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
