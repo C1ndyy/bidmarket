47953ff1029054a1fbf82ca504305ce556481a3b
C1ndyy <cindx.xu@gmail.com>
2021-04-02 22:22:31 -0600

    connect to database

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -8,4 +9,15 @@ def login(request):
     return render(request, 'accounts/login.html')
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'accounts/signup.html', context)
