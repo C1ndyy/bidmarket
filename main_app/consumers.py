c68ca1ffea33ad48fdad13e9d757f871225b98f9
Robin <robin10125@gmail.com>
2021-04-06 12:32:45 -0400

    update

--- main_app/consumers.py
+++ main_app/consumers.py
@@ -4,8 +4,8 @@ class  BidRoomConsumer(AsyncWebsocketConsumer):
     
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat_%s' % self.room_name
-       
+        self.room_group_name = 'bid_%s' % self.room_name
+        print("BidRoomConsumer connect function activate, room_group_name = " + self.room_group_name)
         await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
@@ -18,8 +18,9 @@ class  BidRoomConsumer(AsyncWebsocketConsumer):
             self.room_group_name,
             self.channel_name
         )
+
     async def recieve(Self, text_data):
-        print("BidRoomconsumer recieve function activate")
+        #print("BidRoomconsumer recieve function activate")
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         await self.channel_layer.group_send(
@@ -29,8 +30,9 @@ class  BidRoomConsumer(AsyncWebsocketConsumer):
                 'message': message
             }
         )
-    async def bidroom_mesage(self, event):
-        print("BidRoomConsumer bidroom_message function activate")
+        
+    async def bid_mesage(self, event):
+        print("BidRoomConsumer bid_message function activate")
         message = event['message']
         await self.send(text_data=json.dumps({
             'message': message
