06d582e18a06f1f2e633226df10217979eba0d67
Robin <robin10125@gmail.com>
2021-04-06 23:55:26 -0400

    preparations for merge

--- bidmarket/settings.py
+++ bidmarket/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -107,7 +106,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
