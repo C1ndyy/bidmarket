f68beaf266353526eec47e99edd52bbfd8be5b12
C1ndyy <cindx.xu@gmail.com>
2021-04-08 11:56:44 -0600

    socket fix 3

--- bidmarket/asgi.py
+++ bidmarket/asgi.py
@@ -7,7 +7,7 @@ import main_app.routing
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bid.settings")
 
 application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
+  "https": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             main_app.routing.websocket_urlpatterns
