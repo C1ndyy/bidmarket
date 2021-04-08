a001bc055d8175590ff5859b4840490de3b34572
C1ndyy <cindx.xu@gmail.com>
2021-04-08 10:34:19 -0600

    fix socket

--- main_app/routing.py
+++ main_app/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from . import consumers
  
 websocket_urlpatterns = [
-    re_path(r'ws/bid/(?P<room_name>\w+)/$', consumers.BidConsumer.as_asgi())
+    re_path(r'wss/bid/(?P<room_name>\w+)/$', consumers.BidConsumer.as_asgi())
 ]
